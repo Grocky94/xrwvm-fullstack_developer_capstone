5bf0ec74f71265d93d8688e0320059578f7ef815
Rocky <lastleftdefence@gmail.com>
2026-09-24 02:43:37 -0400

    Added code to post the dealer review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,6 +2,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 load_dotenv()
 
@@ -31,7 +33,36 @@ def get_request(endpoint, **kwargs):
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
+
